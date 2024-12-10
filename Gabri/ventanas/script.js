9ff93b18260a2502fba276e5e6722bb6debb9ac4
chema22022 <147378059+chema22022@users.noreply.github.com>
2024-12-10 13:41:30 +0100

    A
    A

--- Gabri/ventanas/script.js
+++ Gabri/ventanas/script.js
@@ -13,7 +13,7 @@ function pedirCoordenadas() {
       const nuevasX = prompt('Ingrese la nueva coordenada X:', x);
       const nuevasY = prompt('Ingrese la nueva coordenada Y:', y);
 
-      ventana.moveTo(nuevasX,nuevasY);
+      ventana.moveTo(nuevasX, nuevasY);
    } else {
       alert('La ventana secundaria no está abierta.');
    }
@@ -23,15 +23,15 @@ function aplicarColor() {
    const colorSeleccionado = select.value;
    ventana.document.body.style.backgroundColor = colorSeleccionado;
 }
-function cerrarVentanaTiempo(){
+function cerrarVentanaTiempo() {
    var tiempo = prompt("Ingresa la cantidad de segundos para que la ventana esté activa:");
-   tiempo*=1000;
-   setTimeout("ventana.close()",tiempo);
+   tiempo *= 1000;
+   setTimeout("ventana.close()", tiempo);
 }
 function focusVentana() {
    ventana.focus();
 }
-function blurVentana(){
+function blurVentana() {
    ventana.blur();
 }
 function escrbirTexto() {
@@ -40,35 +40,35 @@ function escrbirTexto() {
 }
 var x; // Variable global para el intervalo
 function actualizarContador(duracion) {
-var horaFinal = new Date().getTime() + duracion * 1000;
-x = setInterval(function() {
-var horaActual = new Date().getTime();
-var distancia = horaFinal - horaActual;
-var dias = Math.floor(distancia / (1000 * 60 * 60 * 24));//Redondeamos convirtiendo a milisegundos
-var horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));//Redondeamos convirtiendo a milisegundos
-var minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));//Redondeamos convirtiendo a milisegundos
-var segundos = Math.floor((distancia % (1000 * 60)) / 1000);//Redondeamos convirtiendo a milisegundos
-document.getElementById("countdown").innerHTML = dias + horas + minutos +segundos;
-if (distancia < 0) {//Poner a 0 cuando llega a 0 y mostrar una alerta por pantalla
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "0h 0m 0s";
-    window.alert("Fin boom al lio");
-}
-}, 1000);
+   var horaFinal = new Date().getTime() + duracion * 1000;
+   x = setInterval(function () {
+      var horaActual = new Date().getTime();
+      var distancia = horaFinal - horaActual;
+      var dias = Math.floor(distancia / (1000 * 60 * 60 * 24));//Redondeamos convirtiendo a milisegundos
+      var horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));//Redondeamos convirtiendo a milisegundos
+      var minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));//Redondeamos convirtiendo a milisegundos
+      var segundos = Math.floor((distancia % (1000 * 60)) / 1000);//Redondeamos convirtiendo a milisegundos
+      document.getElementById("countdown").innerHTML = dias + horas + minutos + segundos;
+      if (distancia < 0) {//Poner a 0 cuando llega a 0 y mostrar una alerta por pantalla
+         clearInterval(x);
+         document.getElementById("countdown").innerHTML = "0h 0m 0s";
+         window.alert("Fin boom al lio");
+      }
+   }, 1000);
 }
 function configurarCuentaRegresiva() {
-//Asociamos los datos introducidos con las variables
-//Convertimos a enteros para poder operar con ellas
-var horas = parseInt(document.getElementById("horas").value) || 0;
-var minutos = parseInt(document.getElementById("minutos").value) || 0;
-var segundos = parseInt(document.getElementById("segundos").value) || 0;
-//Convertimos a segundos
-var duracion = (horas * 3600) + (minutos * 60) + segundos;
-//Mostrar mensaje si la cantidad no es válida
-if (duracion > 0) {
-    clearInterval(x);
-actualizarContador(duracion);
- } else {
-    alert("Por favor, introduce una duración válida.");
-}
-}vc
+   //Asociamos los datos introducidos con las variables
+   //Convertimos a enteros para poder operar con ellas
+   var horas = parseInt(document.getElementById("horas").value) || 0;
+   var minutos = parseInt(document.getElementById("minutos").value) || 0;
+   var segundos = parseInt(document.getElementById("segundos").value) || 0;
+   //Convertimos a segundos
+   var duracion = (horas * 3600) + (minutos * 60) + segundos;
+   //Mostrar mensaje si la cantidad no es válida
+   if (duracion > 0) {
+      clearInterval(x);
+      actualizarContador(duracion);
+   } else {
+      alert("Por favor, introduce una duración válida.");
+   }
+} vc
